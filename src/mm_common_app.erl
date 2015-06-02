df5973a260709699c7bba645426d3fbda4d81498
David.Gao <david.alpha.fox@gmail.com>
2015-06-02 17:30:24 +0800

    init project

---
+++ src/mm_common_app.erl
@@ -0,0 +1,16 @@
+-module(mm_common_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    mm_common_sup:start_link().
+
+stop(_State) ->
+    ok.
