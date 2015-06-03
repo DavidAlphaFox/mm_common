6b285e60578602e9defc6998e34647a1693c9b1d
David.Gao <david.alpha.fox@gmail.com>
2015-06-03 18:24:43 +0800

    format code

--- src/mm_date_http.erl
+++ src/mm_date_http.erl
@@ -6,11 +6,11 @@ parse(Date)->
 	try
     	case DateType of
 	    	$\, ->
-		 		mm_date_rfc1123:parse(Date);
+				mm_date_rfc1123:parse(Date);
 	     	$\  ->
 	     		mm_date_asctime:parse(Date);
 	     	_ ->
-		 		mm_date_rfc1036:parse(Date)
+				mm_date_rfc1036:parse(Date)
 		end
 	catch 
 		_Any:_Reason->
