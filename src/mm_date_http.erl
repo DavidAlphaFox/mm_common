c2c80cac4f3790888ec17368f65cb65ce256e05b
David.Gao <david.alpha.fox@gmail.com>
2015-06-03 18:41:59 +0800

    format code of mm_date_http

--- src/mm_date_http.erl
+++ src/mm_date_http.erl
@@ -7,12 +7,12 @@ parse(Date)->
         case DateType of
             $\, ->
                 mm_date_rfc1123:parse(Date);
-	     	$\  ->
+            $\  ->
                 mm_date_asctime:parse(Date);
-	     	_ ->
+            _ ->
                 mm_date_rfc1036:parse(Date)
         end
     catch
         _Any:_Reason ->
             bad_date
-	end.
+    end.
