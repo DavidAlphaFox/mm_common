b5361c66d72ee54f32a4b4e9fdd202138dc28ef3
David.Gao <david.alpha.fox@gmail.com>
2015-06-03 18:40:23 +0800

    format code of mm_date_http

--- src/mm_date_http.erl
+++ src/mm_date_http.erl
@@ -2,17 +2,17 @@
 -export([parse/1]).
 
 parse(Date)->
-	[_D,_A,_Y,DateType| _Rest] = Date,
-	try
-    	case DateType of
-	    	$\, ->
+    [_D,_A,_Y,DateType| _Rest] = Date,
+    try
+        case DateType of
+            $\, ->
                 mm_date_rfc1123:parse(Date);
 	     	$\  ->
                 mm_date_asctime:parse(Date);
 	     	_ ->
-				mm_date_rfc1036:parse(Date)
-		end
-	catch 
-		_Any:_Reason->
+                mm_date_rfc1036:parse(Date)
+        end
+    catch
+        _Any:_Reason ->
             bad_date
 	end.
